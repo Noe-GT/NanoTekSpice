80853720c5459506ced0e8989cc5f45fda8dd696
Killian QUINTIN <killian.quintin@epitech.eu>
2025-02-15 13:43:50 +0100

    [FEAT] added parsing functions recognition to check if a line from the input file is correct

--- include/Parsing.hpp
+++ include/Parsing.hpp
@@ -11,9 +11,31 @@
 #include <sstream>
 #include <string>
 #include <iostream>
+#include <vector>
 #include "Exception.hpp"
 
 namespace nts {
+    class Chipset {
+        public:
+            Chipset(const std::string &type, const std::string &name);
+            ~Chipset();
+
+        private:
+            const std::string _type;
+            const std::string _name;
+    };
+
+    class Link {
+        public:
+            Link(const std::string &c1, std::size_t pin1,
+            const std::string &c2, std::size_t pin2);
+            ~Link();
+
+        private:
+            const std::pair<std::string, std::size_t> _component1;
+            const std::pair<std::string, std::size_t> _component2;
+    };
+
     class Parsing {
         public:
             Parsing();
@@ -23,10 +45,20 @@ namespace nts {
             void getFile();
             void printFile() const;
             void printFile(std::size_t n) const;
+            std::string &cleanStr(std::string &str);
+            std::string &delComments(std::string &line);
+            std::size_t getStringStreamLength(std::stringstream &ss);
+            std::size_t getCharOcc(std::string &str, char c);
+            bool isStrAlnum(std::string &str);
+            bool isStrNum(std::string &str);
+            bool isChipset(std::string &line);
+            bool isLink(std::string &line);
 
         private:
             std::string _filePath;
             std::string _data;
+            std::vector<Chipset> _chipsets;
+            std::vector<Link> _links;
     };
 }
 
--- src/Parsing.cpp
+++ src/Parsing.cpp
@@ -59,3 +59,120 @@ void nts::Parsing::printFile(std::size_t n) const
         n--;
     }
 }
+
+std::string &nts::Parsing::cleanStr(std::string &str)
+{
+    std::string ans("");
+    std::stringstream ss(str);
+    std::string tmp;
+
+    while (ss >> tmp)
+        ans = ans + tmp + " ";
+    if (ans.length() > 0)
+        ans.resize(ans.length() - 1);
+    if (ans == "")
+        ans.clear();
+    return ans;
+}
+
+std::string &nts::Parsing::delComments(std::string &line)
+{
+    std::size_t ind = line.find('#');
+
+    if (ind == std::string::npos)
+        line.clear();
+    else
+        line.resize(ind);
+    return line;
+}
+
+std::size_t nts::Parsing::getStringStreamLength(std::stringstream &ss)
+{
+    std::size_t len = 0;
+    std::string tmp;
+
+    while (ss >> tmp)
+        len++;
+    return len;
+}
+
+std::size_t nts::Parsing::getCharOcc(std::string &str, char c)
+{
+    std::size_t n = 0;
+
+    for (int i = 0; i < str.length(); i++)
+        if (str[i] == c)
+            n++;
+    return n;
+}
+
+bool nts::Parsing::isStrAlnum(std::string &str)
+{
+    for (std::size_t i = 0; i < str.length(); i++)
+        if (!std::isalnum(str[i]))
+            return false;
+    return true;
+}
+
+bool nts::Parsing::isStrNum(std::string &str)
+{
+    for (std::size_t i = 0; i < str.length(); i++)
+        if (!std::isdigit(str[i]))
+            return false;
+    return true;
+}
+
+bool nts::Parsing::isChipset(std::string &line)
+{
+    std::stringstream ss(line);
+    std::string tmp;
+
+    if (this->getStringStreamLength(ss) != 2)
+        return false;
+    for (int i = 0; i < 2; i++) {
+        ss >> tmp;
+        if (!this->isStrAlnum(tmp))
+            return false;
+    }
+    return true;
+}
+
+bool nts::Parsing::isLink(std::string &line)
+{
+    std::stringstream ss(line);
+    std::string tmp;
+    std::string name;
+    std::string pinId;
+
+    if (this->getStringStreamLength(ss) != 2)
+        return false;
+    for (int i = 0; i < 2; i++) {
+        ss >> tmp;
+        if (this->getCharOcc(tmp, ':') != 1)
+            return false;
+        name = tmp.substr(0, tmp.find(':'));
+        pinId = tmp.substr(tmp.find(':') + 1);
+        if (name.empty() || this->isStrAlnum(name) || pinId.empty() ||
+        this->isStrNum(pinId))
+            return false;
+    }
+    return true;
+}
+
+nts::Chipset::Chipset(const std::string &type, const std::string &name):
+    _type(type), _name(name)
+{
+}
+
+nts::Chipset::~Chipset()
+{
+}
+
+nts::Link::Link(const std::string &c1, std::size_t pin1, const std::string &c2,
+    std::size_t pin2): _component1(c1, pin1), _component2(c2, pin2)
+{
+}
+
+nts::Link::~Link()
+{
+}
